75fdd44877dcb010df76faee4098452977fdf419
Kneba <abenkenary3@gmail.com>
2023-05-28 07:54:45 +0000

    ci: initial script
    Signed-off-by: Kneba <abenkenary3@gmail.com>

---
+++ build.sh
@@ -0,0 +1,161 @@
+#!/usr/bin/env bash
+#
+# Copyright (C) 2023 Kneba <abenkenary3@gmail.com>
+#
+
+# Function to show an informational message
+msg() {
+	echo
+    echo -e "\e[1;32m$*\e[0m"
+    echo
+}
+
+err() {
+    echo -e "\e[1;41m$*\e[0m"
+}
+
+cdir() {
+	cd "$1" 2>/dev/null || \
+		err "The directory $1 doesn't exists !"
+}
+
+# Main
+MainPath="$(pwd)"
+MainClangPath="${MainPath}/clang"
+MainClangZipPath="${MainPath}/clang-zip"
+ClangPath="${MainClangZipPath}"
+GCCaPath="${MainPath}/GCC64"
+GCCbPath="${MainPath}/GCC32"
+MainZipGCCaPath="${MainPath}/GCC64-zip"
+MainZipGCCbPath="${MainPath}/GCC32-zip"
+
+# Identity
+VERSION=9x13
+KERNELNAME=TheOneMemory
+CODENAME=Hayzel
+VARIANT=HMP
+
+# Show manufacturer info
+MANUFACTURERINFO="ASUSTek Computer Inc."
+
+# Clone Kernel Source
+git clone --depth=1 https://$USERNAME:$TOKEN@github.com/Kneba/android_kernel_asus_sdm660-4.4 -b lineage-18.1 kernel
+
+# Clone AOSP Clang
+ClangPath=${MainClangPath}
+[[ "$(pwd)" != "${MainPath}" ]] && cd "${MainPath}"
+mkdir $ClangPath
+rm -rf $ClangPath/*
+git clone --depth=1 https://github.com/pkm774/android-kernel-tools $ClangPath
+#git clone --depth=1 https://gitlab.com/ImSurajxD/clang-r450784d -b master $ClangPath
+#wget -q https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+archive/master/clang-r458507.tar.gz -O "clang-r458507.tar.gz"
+#tar -xf clang-r458507.tar.gz -C $ClangPath
+
+# Clone GCC
+#mkdir $GCCaPath
+#mkdir $GCCbPath
+#wget -q https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/+archive/refs/tags/android-12.1.0_r27.tar.gz -O "gcc64.tar.gz"
+#tar -xf gcc64.tar.gz -C $GCCaPath
+#wget -q https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9/+archive/refs/tags/android-12.1.0_r27.tar.gz -O "gcc32.tar.gz"
+#tar -xf gcc32.tar.gz -C $GCCbPath
+
+# Prepared
+KERNEL_ROOTDIR=$(pwd)/kernel # IMPORTANT ! Fill with your kernel source root directory.
+#export LD=ld.lld
+export KBUILD_BUILD_USER=queen # Change with your own name or else.
+IMAGE=$(pwd)/kernel/out/arch/arm64/boot/Image.gz-dtb
+#CLANG_VER="$("$ClangPath"/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')"
+#LLD_VER="$("$ClangPath"/bin/ld.lld --version | head -n 1)"
+#export KBUILD_COMPILER_STRING="$CLANG_VER with $LLD_VER"
+DATE=$(date +"%F-%S")
+START=$(date +"%s")
+
+# Java
+command -v java > /dev/null 2>&1
+
+# Telegram
+export BOT_MSG_URL="https://api.telegram.org/bot$TG_TOKEN/sendMessage"
+
+# Telegram messaging
+tg_post_msg() {
+  curl -s -X POST "$BOT_MSG_URL" -d chat_id="$TG_CHAT_ID" \
+    -d "disable_web_page_preview=true" \
+    -d "parse_mode=html" \
+    -d text="$1"
+}
+# Compiler
+compile(){
+cd ${KERNEL_ROOTDIR}
+export HASH_HEAD=$(git rev-parse --short HEAD)
+export COMMIT_HEAD=$(git log --oneline -1)
+make -j$(nproc) O=out ARCH=arm64 X00TD_defconfig
+make -j$(nproc) ARCH=arm64 SUBARCH=arm64 O=out \
+    LD_LIBRARY_PATH="${ClangPath}/sdclang/linux-x86_64/lib64:${LD_LIBRARY_PATH}" \
+    PATH=$ClangPath/sdclang/linux-x86_64/bin:${PATH} \
+    CC=clang \
+    CROSS_COMPILE=aarch64-linux-gnu- \
+    CROSS_COMPILE_ARM32=arm-linux-gnueabi-
+
+   if ! [ -a "$IMAGE" ]; then
+	finerr
+	exit 1
+   fi
+   git clone --depth=1 https://github.com/strongreasons/AnyKernel3 -b hmp-12 AnyKernel
+	cp $IMAGE AnyKernel
+}
+# Push kernel to telegram
+function push() {
+    cd AnyKernel
+    curl -F document="@$ZIP_FINAL.zip" "https://api.telegram.org/bot$TG_TOKEN/sendDocument" \
+        -F chat_id="$TG_CHAT_ID" \
+        -F "disable_web_page_preview=true" \
+        -F "parse_mode=html" \
+        -F caption="🔐<b>Build Done</b>
+        - <code>$((DIFF / 60)) minute(s) $((DIFF % 60)) second(s)... </code>
+
+        <b>📅 Build Date: </b>
+        -<code>$DATE</code>
+
+        <b>🐧 Linux Version: </b>
+        -<code>4.4.302</code>
+
+         <b>💿 Compiler: </b>
+        -<code>$KBUILD_COMPILER_STRING</code>
+
+        <b>📱 Device: </b>
+        -<code>$DEVICE_CODENAME($MANUFACTURERINFO)</code>
+
+        <b>🆑 Changelog: </b>
+        - <code>$COMMIT_HEAD</code>
+        <b></b>
+        #TheOneMemory #Hayzel #HMP"
+}
+# Find Error
+function finerr() {
+    curl -s -X POST "https://api.telegram.org/bot$TG_TOKEN/sendMessage" \
+        -d chat_id="$TG_CHAT_ID" \
+        -d "disable_web_page_preview=true" \
+        -d "parse_mode=markdown" \
+        -d text="❌ Tetap menyerah...Pasti bisa!!!"
+    exit 1
+}
+# Zipping
+function zipping() {
+    cd AnyKernel || exit 1
+    zip -r9 [$VERSION]$KERNELNAME-$CODENAME-$VARIANT-"$DATE" . -x ".git*" -x "README.md" -x "*.zip"
+
+    ZIP_FINAL="[$VERSION]$KERNELNAME-$CODENAME-$VARIANT-$DATE"
+
+    msg "|| Signing Zip ||"
+    tg_post_msg "<code>🔑 Signing Zip file with AOSP keys..</code>"
+
+    curl -sLo zipsigner-3.0.jar https://github.com/Magisk-Modules-Repo/zipsigner/raw/master/bin/zipsigner-3.0-dexed.jar
+    java -jar zipsigner-3.0.jar "$ZIP_FINAL".zip "$ZIP_FINAL"-signed.zip
+    ZIP_FINAL="$ZIP_FINAL-signed"
+    cd ..
+}
+compile
+zipping
+END=$(date +"%s")
+DIFF=$(($END - $START))
+push
